58714b745044c532f83e9f9a0ebfe0452bbe115a
Mikołaj <61738937+MikolajJ420@users.noreply.github.com>
2021-04-21 19:36:04 +0200

    Update skrypt.js

--- EE.09-02-20.01-SG/skrypt.js
+++ EE.09-02-20.01-SG/skrypt.js
@@ -1,6 +1,6 @@
 function oblicz(){
 	var a = +document.getElementById("dystans").value;
-    var b = +document.getElementById("spalanie").value;
-    var wynik = a * (b/100);
-    document.getElementById("wynik").innerHTML= "Potrzebujesz:  "+ wynik +" litrów paliwa";
+	var b = +document.getElementById("spalanie").value;
+	var wynik = a * (b/100);
+	document.getElementById("wynik").innerHTML= "Potrzebujesz:  "+ wynik +" litrów paliwa";
 }
